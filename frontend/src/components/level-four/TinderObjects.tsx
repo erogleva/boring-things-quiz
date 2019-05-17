60cd29e237bfb94011000b5031e98653ff03538c
erogleva <e.rogleva@mail.bg>
2019-05-17 21:08:36 +0200

    Show an error if the server error does not succeed

--- frontend/src/components/level-four/TinderObjects.tsx
+++ frontend/src/components/level-four/TinderObjects.tsx
@@ -27,9 +27,11 @@ const TinderObjects = (props: Props) => {
         });
 
         setLikesPercentage(null);
+        setError(false);
     };
 
     const handleReaction = async (type: string) => {
+        setError(false);
         let res;
         const id = objects[currentObject].id;
 
@@ -43,28 +45,28 @@ const TinderObjects = (props: Props) => {
             const likes = Number.parseInt(res.data.likes, 10);
             const dislikes = Number.parseInt(res.data.dislikes, 10);
 
-            console.log(res);
-
             const totalVotes = likes + dislikes;
 
             const likesPercentage = Math.trunc((likes / totalVotes) * 100);
 
             setLikesPercentage(likesPercentage);
         } catch (e) {
-            console.log(e)
+            setError(true)
         }
     };
 
     const [currentObject, setCurrentObject] = useState(0);
     const [likesPercentage, setLikesPercentage] = useState<null|number>(null);
+    const [error, setError] = useState(false);
 
     const renderTitle = 'h6';
     return <div className='level-four'>
-        <Trans render={renderTitle}>Jetzt darft du uns mal deine Meinung sagen! Findest du das Objekt
-            "langweilig"?</Trans>
+        <Trans render={renderTitle}>Jetzt darfst du uns mal deine Meinung sagen! Findest du das Objekt
+            "langweilig" oder nicht?</Trans>
         <img src={require(`../../assets/img/${objects[currentObject].src}`)}
              alt={objects[currentObject].name}/>
         {likesPercentage !== null && <Trans render={renderTitle}>{likesPercentage} Prozent der Nutzer fanden dieses Objekt nicht langweilig!</Trans>}
+        {error && <Trans render={renderTitle}> <span className='error'>Auf dem Server ist ein interner Fehler aufgetreten.</span></Trans>}
         <div className='reactions'>
             <div className='bored'>
                 <img src={require('../../assets/img/level-four/bored-reaction.png')} alt="bored" onClick={() => handleReaction(reactionTypes.DISLIKE)}/>

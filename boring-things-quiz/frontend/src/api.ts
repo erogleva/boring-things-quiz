fb75016cbe524be6788105a8fbc959bfb48f2a0d
erogleva <e.rogleva@mail.bg>
2019-06-27 14:33:33 +0200

    Correct url

--- boring-things-quiz/frontend/src/api.ts
+++ boring-things-quiz/frontend/src/api.ts
@@ -3,10 +3,10 @@ import axios from 'axios';
 const backend = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:5000';
 
 export async function likeObject(id: string) {
-    return await axios.put(`${backend}/${id}/like`, {})
+    return await axios.put(`${backend}/items/${id}/like`, {})
 }
 
 export async function dislikeObject(id: string) {
-    return await axios.put(`${backend}/${id}/dislike`, {})
+    return await axios.put(`${backend}/items/${id}/dislike`, {})
 }
 

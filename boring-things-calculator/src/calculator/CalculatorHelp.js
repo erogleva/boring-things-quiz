8aa5add6a181f8e9bb1d505901dd9fd76e2c2103
erogleva <e.rogleva@mail.bg>
2019-09-14 18:51:41 +0200

    Make the calculator work as a PWA with Safari

--- boring-things-calculator/src/calculator/CalculatorHelp.js
+++ boring-things-calculator/src/calculator/CalculatorHelp.js
@@ -9,7 +9,8 @@ const CalculatorHelp = (props) => {
 
     return <div className='calculator-help-page'>
 
-        <video id="calculator-video" controls="true" width="100%" height="100%" playsinline preload="metadata" poster={require('./data/Rechenmaschine_VideoPreview.png')}>
+        {/*<iframe title='calculator-video' width="100%" height="100%" src="https://drive.google.com/file/d/1YSKAfQmhA2OorIXsQEwgrzFxg8YqSpqZ/preview"/>*/}
+        <video crossOrigin='anonymous' id="calculator-video" controls="true" width="100%" height="100%" playsinline poster={require('./data/Rechenmaschine_VideoPreview.png')}>
             <source src={require('./data/screencast_calculator.mp4')} type="video/mp4" />
             <p>This browser does not support the video element.</p>
         </video>
---
+++ boring-things-calculator/src/sw.js
@@ -0,0 +1,40 @@
+workbox.setConfig({ debug: true });
+
+
+workbox.core.setCacheNameDetails({
+    prefix: 'boring-things-calculator',
+    suffix: 'v2',
+    precache: 'precache',
+    runtime: 'runtime'
+});
+
+self.addEventListener('install', (event) => {
+
+    const screencast = self.__precacheManifest.find((item) => item.url.includes('screencast'));
+
+    if (screencast) {
+        console.log(screencast)
+        const urls = [screencast.url];
+        const cacheName = 'videos';
+        event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
+    }
+
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest.filter((item) => !item.url.includes('screencast')) || []);
+
+
+workbox.routing.registerRoute(
+    /'https:\/\/cdn.*/,
+    new workbox.strategies.NetworkFirst(),
+);
+
+workbox.routing.registerRoute(
+    /.*\.mp4/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'videos',
+        plugins: [
+            new workbox.rangeRequests.Plugin(),
+        ],
+    }),
+);
